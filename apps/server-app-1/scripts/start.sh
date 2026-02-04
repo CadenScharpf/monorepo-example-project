41337ce99fedba95b27eedf73dfdd2960a7184f6
Caden <u16scharpf@gmail.com>
2026-02-03 20:42:24 -0700

    cp

--- apps/server-app-1/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-# Defaults
-APP_PORT="${PORT:-80}"
-
-echo "Starting app on port $APP_PORT"
-exec node ./bin/www
---
+++ apps/server-app-1/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (_req: Request, res: Response) => {
+  res.json({ ok: true });
+});
+
+const port = Number(process.env.PORT ?? 3000);
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
